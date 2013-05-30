3e8eed9d589f3f79fc9a7ec26db894280681871a
shelion <chavi.rg@gmail.com>
2013-05-30 00:53:36 -0600

    Create temperature.rb

---
+++ 01_temperature/temperature.rb
@@ -0,0 +1,14 @@
+#01 temperature
+
+#ftoc
+#convert Fahrenheit to Celsius
+
+def ftoc(fahrenheit)
+ (fahrenheit - 32) * (5.0/9.0)
+end
+
+#ctof
+#convert Celsius to Fahrenheit
+def ctof(celsius)
+ (celsius * (9.0/5.0)) +32
+end
